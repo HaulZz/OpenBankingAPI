acfb309559ba4767a4abc198ae63c536d3e43249
Jiahao Zhao <50120736+HaulZz@users.noreply.github.com>
2021-06-10 15:41:02 -0300

    first commit

---
+++ Models/AppData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class AppData
+    {
+        public int ClientId { get; set; }
+        public string BaseUrl { get; set; }
+        public string BaseAuthUrl { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
