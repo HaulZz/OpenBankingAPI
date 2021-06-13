5359ee1b63cc03c8ec671bc45751ce71fbff4478
Jiahao Zhao <50120736+HaulZz@users.noreply.github.com>
2021-06-13 16:51:15 -0300

    auth1

--- Services/Teste.cs
+++ Services/Teste.cs
@@ -9,6 +9,12 @@ using JWT.Serializers;
 using System.IO;
 using System.Security.Claims;
 using System.Security.Cryptography;
+using ThirdParty.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using AutoFixture;
+using JWT.Builder;
 
 namespace WebApplication1.Services
 {
@@ -21,128 +27,73 @@ namespace WebApplication1.Services
                 " END RSA PRIVATE KEY---- - ";
         }
 
-        public static void Test()
+
+
+        public static string PrivateKey { get; set; }
+        public static AppData GetApplicationData()
         {
+            AppData appData = new AppData()
+            {
+                ClientId = 646546546,
+                BaseUrl = "",
+                BaseAuthUrl = "",
+                PrivateKey = ""
+            };
+            return appData;
+        }
+
+        public static void GetToken()
+        {
+            AppData appData = GetApplicationData();
+
+            var baseUrl = $"{appData.BaseAuthUrl}/auth/realms/stone_bank";
+            var authUrl = $"{baseUrl}/protocol/openid-connect/token";
+
             string publicKey = File.ReadAllText(@"C:\Users\JiaHaoZhao\source\repos\WebApplication1\mykey.pem");
             string privateKey = File.ReadAllText(@"C:\Users\JiaHaoZhao\source\repos\WebApplication1\mykey.pub");
 
-            var claims = new List<Claim>();
-            claims.Add(new Claim("claim1", "value1"));
-            claims.Add(new Claim("claim2", "value2"));
-            claims.Add(new Claim("claim3", "value3"));
-
-            var token = CreateToken(claims, privateKey);
-            var payload = DecodeToken(token, publicKey);
-        }
-
-        public static string CreateToken(List<Claim> claims, string privateRsaKey)
-        {
-            RSAParameters rsaParams;
-            using (var tr = new StringReader(privateRsaKey))
+            var payload = new Payload()
             {
-                var pemReader = new PemReader(tr);
-                var keyPair = pemReader.ReadObject() as AsymmetricCipherKeyPair;
-                if (keyPair == null)
-                {
-                    throw new Exception("Could not read RSA private key");
-                }
-                var privateRsaParams = keyPair.Private as RsaPrivateCrtKeyParameters;
-                rsaParams = DotNetUtilities.ToRSAParameters(privateRsaParams);
-            }
-            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
-            {
-                rsa.ImportParameters(rsaParams);
-                Dictionary<string, object> payload = claims.ToDictionary(k => k.Type, v => (object)v.Value);
-                return Jose.JWT.Encode(payload, rsa, Jose.JwsAlgorithm.RS256);
-            }
-        }
+                ClientId = appData.ClientId,
+                Exp = 321321,
+                Nbf = 32132132,
+                Aud = baseUrl,
+                Realm = "Stone_bank",
+                Sub = appData.ClientId,
+                Jti = "tempo",
+                Iat = 321321
+            };
 
-        public static string DecodeToken(string token, string publicRsaKey)
+            var token = jwt.encode(payload, app_data.private_key, algorithm = 'RS256')
+
+        object header =
         {
-             RSAParameters rsaParams;
+            "Content-Type" : "application/x-www-form-urlencoded",
+            "User-Agent": "Insira aqui o Nome da Aplicação"
+        };
 
-             using (var tr = new StringReader(publicRsaKey))
-             {
-                  var pemReader = new PemReader(tr);
-                  var publicKeyParams = pemReader.ReadObject() as RsaKeyParameters;
-                  if (publicKeyParams == null)
-                  {
-                       throw new Exception("Could not read RSA public key");
-                  }
-                  rsaParams = DotNetUtilities.ToRSAParameters(publicKeyParams);
-             }
-             using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
-             {
-                  rsa.ImportParameters(rsaParams);
-                  // This will throw if the signature is invalid
-                  return Jose.JWT.Decode(token, rsa, Jose.JwsAlgorithm.RS256);  
-             }
-        }
+        var payload = new Dictionary<string, object>
+        {
+            { "exp", 123456 },
+            { "nbf", 123 },
+            { "aud",  baseUrl},
+            { "realm", "stone_bank"},
+            { "sub", appData.ClientId },
+            { "clientId", appData.ClientId },
+            { "jti", "time" },
+            { "iat", 123456 }
+        };
+        private static readonly Fixture _fixture = new Fixture();
+        var publicKey = _fixture.Create<RSACryptoServiceProvider>();
 
-        //public static string PrivateKey { get; set; }
-        //public static AppData GetApplicationData()
-        //{
-        //    AppData appData = new AppData()
-        //    {
-        //        ClientId = 646546546,
-        //        BaseUrl = "",
-        //        BaseAuthUrl = "",
-        //        PrivateKey = ""
-        //    };
-        //    return appData;
-        //}
+        var token = JwtBuilder.Create()
+                              .WithAlgorithm(new RS256Algorithm(publicKey, privateKey)) // symmetric
+                              .WithSecret(secret)
+                              .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
+                              .AddClaim("claim2", "claim2-value")
+                              .Encode();
 
-        //public static void GetToken()
-        //{
-        //    AppData appData = GetApplicationData();
-
-        //    var baseUrl = $"{appData.BaseAuthUrl}/auth/realms/stone_bank";
-        //    var authUrl = $"{baseUrl}/protocol/openid-connect/token";
-
-        //    string publicKey = File.ReadAllText(@"C:\Users\JiaHaoZhao\source\repos\WebApplication1\mykey.pem");
-        //    string privateKey = File.ReadAllText(@"C:\Users\JiaHaoZhao\source\repos\WebApplication1\mykey.pub");
-
-        //var payload = new Payload()
-        //{
-        //    ClientId = appData.ClientId,
-        //    Exp = 321321,
-        //    Nbf = 32132132,
-        //    Aud = baseUrl,
-        //    Realm = "Stone_bank",
-        //    Sub = appData.ClientId,
-        //    Jti = "tempo",
-        //    Iat = 321321
-        //};
-
-        //var token = jwt.encode(payload, app_data.private_key, algorithm='RS256')
-
-        //object header =
-        //{
-        //    "Content-Type" : "application/x-www-form-urlencoded",
-        //    "User-Agent": "Insira aqui o Nome da Aplicação"
-        //};
-
-        //var payload = new Dictionary<string, object>
-        //{
-        //    { "exp", 123456 },
-        //    { "nbf", 123 },
-        //    { "aud",  baseUrl},
-        //    { "realm", "stone_bank"},
-        //    { "sub", appData.ClientId },
-        //    { "clientId", appData.ClientId },
-        //    { "jti", "time" },
-        //    { "iat", 123456 }
-        //};
-
-        //const string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
-
-        //IJwtAlgorithm algorithm = new RS256Algorithm(privateKey); // symmetric
-        //IJsonSerializer serializer = new JsonNetSerializer();
-        //IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
-        //IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
-
-        //var token = encoder.Encode(payload, secret);
-        //Console.WriteLine(token);
+        Console.WriteLine(token);
 
     }
     }
