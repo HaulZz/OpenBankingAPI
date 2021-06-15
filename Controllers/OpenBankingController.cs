76edacc17edbaf1d9f8e777cfe0dc2b2767a6955
Jiahao Zhao <50120736+HaulZz@users.noreply.github.com>
2021-06-15 14:39:15 -0300

    remove unnecessary code

--- Controllers/OpenBankingController.cs
+++ Controllers/OpenBankingController.cs
@@ -12,7 +12,7 @@ namespace WebApplication1.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StoneController : ControllerBase
+    public class OpenBankingController : ControllerBase
     {
         // GET: api/<ValuesController>
         [HttpGet]
@@ -20,20 +20,6 @@ namespace WebApplication1.Controllers
         {
             //var encrypted = "";
             return WebhookServices.Encrypt();
-        }
-
-
-        // POST api/<ValuesController>
-        [HttpPost]
-        public ActionResult<string> Post(string text)
-        {
-            return WebhookServices.Decrypt();
-        }
-
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
         }
     }
 }
---
+++ Controllers/WebhookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        // POST api/<ValuesController>
+        [HttpPost]
+        public ActionResult<string> Post(string text)
+        {
+            return WebhookServices.Decrypt();
+        }
+
+
+    }
+}
